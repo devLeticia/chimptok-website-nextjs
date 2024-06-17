0ff92afadba327b17b5850c2ea176ce48e95a8f3
Leticia Gonçalves <leticiagoncalves.tech@gmail.com>
2024-06-17 20:32:37 -0300

    enh next config for deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true, 
+    swcMinify: true, 
+    output:'export',
+    images: {
+        unoptimized: true
+      },
     swcMinify: true,
     // fastRefresh: true,
     // concurrentFeatures: true,
